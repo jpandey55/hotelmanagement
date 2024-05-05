1597d78102557589e633d03fca5baeb94b2aac9c
jpandey55 <pandeyjitendra5994@gmail.com>
2024-05-05 01:51:51 -0400

    Initial Commit

---
+++ src/main/java/com/engineers/hotelmanagement/HotelmanagementApplication.java
@@ -0,0 +1,15 @@
+package com.engineers.hotelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelmanagementApplication.class, args);
+		System.out.println("Hello I am here");
+	}
+
+
+}
